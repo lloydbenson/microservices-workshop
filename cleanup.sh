c65e6a9c513148cf2108fc0864b553d48a4f44e8
Wyatt Preul <wpreul@gmail.com>
2016-08-03 13:36:14 -0500

    Fix challenge 10

--- cleanup.sh
+++ cleanup.sh
@@ -4,12 +4,13 @@ echo "Stopping all containers"
 docker stop $(docker ps -a -q)
 echo "Removing all containers"
 docker rm $(docker ps -a -q)
-echo "Removing any none images"
-docker rmi $(docker images | grep '<none>' | awk '{print $3}') 
+echo "Removing any <none> images"
+docker rmi $(docker images | grep '<none>' | awk '{print $3}')
 echo "Removing fuge images"
-docker rmi $(docker images | grep 'fuge_' | awk '{print $3}') 
-echo "Removing tutum image"
-docker rmi $(docker images | grep '^tutum' | awk '{print $3}') 
-echo "Removing node image"
-docker rmi $(docker images | grep '^node' | awk '{print $3}') 
+docker rmi $(docker images | grep 'fuge_' | awk '{print $3}')
 
+
+# echo "Removing tutum image"
+# docker rmi $(docker images | grep '^tutum' | awk '{print $3}')
+# echo "Removing node image"
+# docker rmi $(docker images | grep '^node' | awk '{print $3}')
